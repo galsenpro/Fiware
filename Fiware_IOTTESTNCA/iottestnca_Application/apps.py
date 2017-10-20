e48aabebc3fd44c707cfc4cf136823eddf0a0be3
DIENG Adama <adama.dieng@nicecotedazur.org>
2017-10-20 11:35:09 +0200

    Application & NCA Datamodels

---
+++ Fiware_IOTTESTNCA/iottestnca_Application/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IottestncaApplicationConfig(AppConfig):
+    name = 'iottestnca_Application'
---
+++ Fiware_IOTTESTNCA/iottestnca_Application/nca.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#from Fiware_IOTTESTNCA.iottestnca.iot.iotSOURCES import *
+from iottestnca_Application.iot.iotSOURCES import *
+if __name__ == "__main__":
+    map =  IOTSOURCE()
+    liste = map.ncaGetListFiles()
+    map.ncaConvertListOfFiles(liste)
