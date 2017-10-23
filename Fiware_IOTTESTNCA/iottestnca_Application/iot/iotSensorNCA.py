1d4bd5408fee4c7422a057a0bd4edf29dbbb499b
DIENG Adama <adama.dieng@nicecotedazur.org>
2017-10-23 16:50:00 +0200

    Sensibel Datamodel

---
+++ Fiware_IOTTESTNCA/iottestnca_Application/iot/iotSensorNCA.py
@@ -0,0 +1,26 @@
+#-*- coding: utf-8 -*-
+
+"""
+    Create Device datamodel for each lines of Sensibel source
+"""
+class IOTSENSORNCA:
+
+    def __init__(self, sensorSourceFile = None , sensibelDM = None):
+        try:
+            print("Sensor NCA ...")
+        except Exception as x:
+            print(x)
+
+    def ncaGetAttributeSensor(self, data, *params):
+        try:
+            print("Getting attributes sensor ...")
+
+        except Exception as x:
+            print(x)
+
+    def ncaMapAttribute(self, attribute):
+        try:
+            print("Mapping ...")
+
+        except Exception as x:
+            print(x)
