023ba11da5eaa447968d6a0513aefff243b8a502
DIENG Adama <adama.dieng@nicecotedazur.org>
2017-10-25 17:26:02 +0200

    Mapping Sensor DM-NCA

--- Fiware_IOTTESTNCA/iottestnca_Application/iot/iotDMSENSOR.py
+++ Fiware_IOTTESTNCA/iottestnca_Application/iot/iotDMSENSOR.py
@@ -2,12 +2,15 @@ from iottestnca_Application.iot.iotSensorNCA import *
 import ast
 class IOTDMSENSOR:
     def __init__(self,sensorTuple, dmSource = "Device-NCA-6452.json"):
-        self.dm = {}
+
         #self.dmSource = json.loads(open(dmSource).read())
         self.sensorFolder = settings.BASE_DIR + settings.SENSORFOLDER
         self.file =self.sensorFolder + dmSource
         #print(json.dumps(open(self.config).read()))
         self.dmSource = json.loads(open(self.file,"r").read())
+        self.dmSource = dict(del self.dmSource["controlledProperty"])
+        #print(self.dmSource)
+        #print(json.dumps(self.dmSource))
         #print(self.dmSource)
         self.sensorTuple = sensorTuple
 
@@ -15,21 +18,20 @@ class IOTDMSENSOR:
         try:
             print("Mapping Sensor ...")
             for tpl in self.sensorTuple:
-                self.dm["idS"] = tpl[1]+"-"+ str(position)
-                self.dm["controlledProperty"] = tpl[2]
-                values = " "
+                dm = {}
+                dm["idS"] = tpl[1]+"-"+ str(position)
+                dm["controlledPropertyS"] = tpl[2]
+                values = ""
                 for i in tpl[2]:
                     myIndex = tpl[2].index(i)
                     values = values + i + "=" +tpl[3][myIndex] +";"
-                self.dm["valueS"] = values
+                dm["value"] = values
                 #print(tpl)
-                #self.dmSource = json.loads(self.dmSource)
-                #self.dm.update(ast.literal_eval(json.dumps(self.dmSource)))
-
-                #print(self.dmSource)
+                dm.update(self.dmSource)
+                #print(self.dm.update(self.dmSource))
                 #self.dm = json.loads(self.dm)
                 #self.dmSource = self.dmSource
-                jsonF = json.dumps(self.dm)
+                jsonF = json.dumps(dm, sort_keys=True, indent=4)
                 print(jsonF)
 
                 #print(json.loads(jsonF))
